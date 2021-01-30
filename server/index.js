329d9bd0328c7ea7bbe7705326e8a2a217febed9
James Liang <james.liang.cje@gmail.com>
2021-01-30 16:01:22 -0500

    implemented server socket functionality to transmit rtc descriptions

--- server/index.js
+++ server/index.js
@@ -9,11 +9,26 @@ const io = require('socket.io')(httpServer, {
 });
 
 io.on('connection', socket => {
-    console.log('connect');
+  console.log('connect');
   socket.on("offer",offer => {
     console.log(offer)
     console.log(`received offer ${offer}`)
   })
+
+  socket.on("join create room",roomName => {
+    const ids = io.sockets.adapter.rooms.get(roomName)
+    socket.emit("participants",ids)
+    socket.join(roomName)
+  })
+
+  socket.on("offer",(fromId,toId,offer) => {
+    socket.to(toId).emit("offer",fromId,offer)
+  })
+
+  socket.on("answer",(fromId,toId,answer) => {
+    socket.to(toId).emit("answer",fromId,answer)
+  })
+
 });
 
 httpServer.listen(3000, () => {
