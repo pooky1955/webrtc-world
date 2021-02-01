2385ee30bdb1b7c2013518287058280feb8af6ca
James Liang <james.liang.cje@icloud.com>
2021-01-31 20:58:25 -0500

    modified backend url

--- frontend/Config.js
+++ frontend/Config.js
@@ -1,2 +1,4 @@
 const DEV_BACKEND_URL = "http://127.0.0.1:3000" 
-const PROD_BACKEND_URL = ""
+const PROD_BACKEND_URL = "https://webrtc-world-backend.herokuapp.com/"
+const BACKEND_URL = window.location.hostname !== "127.0.0.1" ? PROD_BACKEND_URL : DEV_BACKEND_URL
+
